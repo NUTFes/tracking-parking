aa48fa06720fb79daea166643944b240a18670c7
nose221834 <22.t.ichinose.nutfes@gmail.com>
2024-02-08 17:06:48 +0900

    画像が保存されるようにする

--- raspberryPi/create_number_plate.py
+++ raspberryPi/create_number_plate.py
@@ -1,6 +1,12 @@
 from number_plate import NumberPlate
+import cv2
 
 category=0 # 0:普通車（自家用） 1:普通車（事業用） 2:軽自動車（自家用） 3:軽自動車（事業用）
 
 number_plate = NumberPlate()
 img = number_plate.generate(category)
+print(img)
+
+# imgに画像のデータがはいっているから、これを保存すると画像になる
+# 実行するとtest.pngが生成されるからやってみて
+cv2.imwrite('./test.jpg', img)
