15e981ab8a0e0409cfb2d67279f2b4990d7f6f55
nose221834 <iatyy01151821@gmail.com>
2024-09-07 23:34:26 +0900

    学習ファイルを編集

--- yolo_fine_tuning/src/fine_tuning.py
+++ yolo_fine_tuning/src/fine_tuning.py
@@ -1,3 +1,7 @@
+import matplotlib
+matplotlib.use('Agg')  # GUIなしでグラフを処理するバックエンドを使用
+
+
 import os
 
 from dotenv import load_dotenv
@@ -6,11 +10,13 @@ from ultralytics import YOLO
 load_dotenv()
 HOME_DIR = os.environ["HOME_DIR"]
 
-model = YOLO("yolov8s.pt")
+model = YOLO("yolov8m.pt")
 model.train(
     data=HOME_DIR + "/yolo_fine_tuning/yolo/dataset.yaml",
     epochs=5,
     batch=8,
     workers=4,
     degrees=90.0,
+    imgsz=1920,  # 最大の幅を指定
+    rect=True    # 縦横比を保持
 )
