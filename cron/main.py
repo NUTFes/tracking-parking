a357aa202941ac07e7922f244278bd2d945f93f3
nose221834 <iatyy01151821@gmail.com>
2024-09-08 05:01:10 +0900

    コメントを追加

--- cron/main.py
+++ cron/main.py
@@ -1,17 +1,19 @@
-import requests
-from pymongo import MongoClient
-import time
 import os
+
+import requests
 from dotenv import load_dotenv
+from pymongo import MongoClient
 
 # -----------sakuraからデータを取得------------
+# TODO: あとで、send.pyの位置を変えて、importする
 
 # URLを指定
 load_dotenv()
 token = os.environ["TOKEN"]
 module = os.environ["MODULE"]
 channel = os.environ["CHANNEL"]
-url = f'https://api.sakura.io/datastore/v2/channels?token={token}&module={module}&channel={channel}'
+url = f"https://api.sakura.io/datastore/v2/channels?token={token}&module={module}&channel={channel}"
+print(url)
 
 # GETリクエストを送信
 response = requests.get(url)
@@ -28,37 +30,40 @@ else:
     print(f"Failed to fetch data. Status code: {response.status_code}")
 # --------------------------------------------
 
+
 # -----------mongoDBへデータを送信--------------
-def connect_mongo(user,password,host,port,db_name,collection_name):
+def connect_mongo(user, password, host, port, db_name, collection_name):
     client = MongoClient(f"mongodb://{user}:{password}@{host}:{port}")
     db = client[db_name]
     collection = db[collection_name]
-    return collection,client
+    return collection, client
+
 
 def make_doc(num):
     num_int = f"{num}"
     document = {"number": int(num_int)}
     return document
 
-def send_mongo(num,mongo_col):
+
+def send_mongo(num, mongo_col):
     print("send mongo")
     doc = make_doc(num)
     mongo_col.insert_one(doc)
     print("send finish")
-    #mongo_cl.close()
+    # mongo_cl.close()
     return
+
 
 # print(get_time())
 
-if __name__ == "__main__": 
-    
+if __name__ == "__main__":
     USER = os.environ["USER_NAME"]
     PASS = os.environ["PASS"]
     HOST = os.environ["HOST"]
     PORT = os.environ["PORT"]
     DB_NAME = os.environ["DB_NAME"]
     COLLECTION_NAME = os.environ["COLLECTION_NAME"]
-    collection,client = connect_mongo(USER,PASS,HOST,PORT,DB_NAME,COLLECTION_NAME)
+    collection, client = connect_mongo(USER, PASS, HOST, PORT, DB_NAME, COLLECTION_NAME)
     send_mongo(recentData["value"], collection)
     print(collection)
 # --------------------------------------------
